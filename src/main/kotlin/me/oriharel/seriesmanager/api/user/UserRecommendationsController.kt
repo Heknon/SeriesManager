9eb9a0edd5777bb74793b4947746208f8693dbd9
Heknon <development@heknon.com>
2020-10-03 13:10:17 +0300

    fixed

---
+++ src/main/kotlin/me/oriharel/seriesmanager/api/user/UserRecommendationsController.kt
@@ -0,0 +1,9 @@
+package me.oriharel.seriesmanager.api.user
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/user/{userId}/recommend?")
+class UserRecommendationsController {
+}
